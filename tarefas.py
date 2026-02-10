944f766ba71fb094d59b2be0f8fa44b3d60afafe
Emilly <emilly.dev.monteiro@gmail.com>
2026-02-10 16:53:51 -0300

    Adicionando uma função

---
+++ tarefas.py
@@ -0,0 +1,11 @@
+tarefas = []
+
+def mostrar():
+    print("OPÇÔES")
+    print("1 - Adicionar Tarefas")
+    print("2 - Remover Tarefas")
+    print("3 - Listar Tarefas")
+    print("0 - Sair")
+
+
+
